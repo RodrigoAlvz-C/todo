927bb0b0ce0479114d9c8837232a6bec1c7fb177
gorz <c.rodrigoalvz@gmail.com>
2024-09-09 09:53:00 -0300

    projectMannager IIFE

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ import "./styles.css";
 import { Todo } from "./modules/todo";
 import { Project } from "./modules/project";
 
-let defaultProject = Project('default')
+const projectMannager = (() => {
+
+  let list = []
+  
+  const addProject = (name) => {
+    const project = Project(name)
+    list.push(project)
+  }
+  const deleteProject = (index) => list.splice(index, 1)
+  const getProject = (index) => list[index]
+  const getList = () => list
+
+  return {
+    addProject,
+    deleteProject,
+    getProject,
+    getList
+  }
+
+})();
+
+
+projectMannager.addProject('default')
